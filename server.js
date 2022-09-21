d4b4708daa87c0f16ef886eed850870e1e9ff2e0
omarfarah123 <76454677+omarfarah123@users.noreply.github.com>
2022-09-21 09:16:10 -0400

    Added post route for notes

--- server.js
+++ server.js
@@ -35,6 +35,55 @@ app.get('/api/notes', function (req, res) {
 	}
 })
 
+//Post route for adding new notes
+app.post('/api/notes', function (req, res) {
+    try{
+      console.log("Hit the post route")
+      const { title, text } = req.body;
+    
+      if (title && text) {
+        
+        const newNote = {
+          title,
+          text,
+          note_id: uuid(),
+        };
+        console.log(newNote);
+              // Obtain existing notes
+        fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
+          if (err) {
+            console.error(err);
+          } else {
+            // Convert string into JSON object
+            const parsedNotes = JSON.parse(data);
+  
+            parsedNotes.push(newNote);
+  
+            // Write updated notes back to file
+            fs.writeFile(
+              path.join(__dirname, '/db/db.json'),
+              JSON.stringify(parsedNotes, null, 4),
+              (writeErr) =>
+                writeErr
+                  ? console.error(writeErr)
+                  : console.info('Successfully updated notes!')
+            );
+          }
+        });
+    
+        const response = {
+          status: 'success',
+          body: newNote,
+        };
+    
+        console.log(response);
+        res.json(response);
+      }
+    } catch (error) {
+      res.json("problem adding note")
+    }
+  })
+
 
 
 app.listen(PORT, () =>
